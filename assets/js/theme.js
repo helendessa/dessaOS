b825422d203d759a9e623eb2942191d78d1d14f6
helendessa <helendessa@gmail.com>
2025-02-22 18:02:45 -0300

    update themes: selected theme is applied across all pages

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,11 +1,26 @@
-document.getElementById('theme-toggle').addEventListener('click', () => {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    const themeIcon = document.getElementById('theme-icon');
-    if (document.body.classList.contains('dark-theme')) {
-        themeIcon.src = '/assets/images/light-theme-icon.svg'; // on dark theme
+// apply the theme based on local storage
+function applyTheme(theme) {
+    if (theme === 'dark') {
+        document.body.classList.add('dark-theme');
+        document.body.classList.remove('light-theme');
+        document.getElementById('theme-icon').src = '/assets/images/light-theme-icon.svg';
     } else {
-        themeIcon.src = '/assets/images/dark-theme-icon.svg'; // on ligth theme
+        document.body.classList.add('light-theme');
+        document.body.classList.remove('dark-theme');
+        document.getElementById('theme-icon').src = '/assets/images/dark-theme-icon.svg';
     }
+}
+
+// check local storage for theme preference on page load
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    applyTheme(savedTheme);
+});
+
+// toggle theme and save preference to local storage
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    const currentTheme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    applyTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
 });
